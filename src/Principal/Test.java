5369163b4f0f7d3652e75bfe4f52d077900d8e80
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2021-02-24 14:21:41 -0300

    Test on firts commit

---
+++ src/Principal/Test.java
@@ -0,0 +1,15 @@
+package Principal;
+
+import javax.swing.*;
+
+public class Test {
+    private JPanel jpanelPrincipal;
+    private JLabel labelOlaMundo;
+
+    public static void main(String[] args) {
+    JFrame fram = new JFrame("Ola Mundo");
+    fram.setVisible(true);
+    }
+}
+
+
